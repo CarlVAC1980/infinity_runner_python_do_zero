b9a5486ccf86fb57eaa621c031f459f499bf7fba
Carlos M Valenca <cavac10@gmail.com>
2022-08-17 21:32:56 -0300

    Atualização do projeto 23:01 comportamento dos componentes

--- main.py
+++ main.py
@@ -2,26 +2,76 @@ import pygame
 
 WIDTH = 1200
 HEIGHT = 600
+SPEED = 1
+GAME_SPEED = 10
+GROUND_WIDTH = 2 * WIDTH
+GROUND_HEIGHT = 30
+
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load('sprite/Run_000.png')
+        self.image_run = [pygame.image.load('sprites/Run__000.png').convert_alpha(),
+                          pygame.image.load('sprites/Run__001.png').convert_alpha(),
+                          pygame.image.load('sprites/Run__002.png').convert_alpha(),
+                          pygame.image.load('sprites/Run__003.png').convert_alpha(),
+                          pygame.image.load('sprites/Run__004.png').convert_alpha(),
+                          pygame.image.load('sprites/Run__005.png').convert_alpha(),
+                          pygame.image.load('sprites/Run__006.png').convert_alpha(),
+                          pygame.image.load('sprites/Run__007.png').convert_alpha(),
+                          pygame.image.load('sprites/Run__008.png').convert_alpha(),
+                          pygame.image.load('sprites/Run__009.png').convert_alpha(),
+                          ]
+        self.image = pygame.image.load('sprites/Run__000.png').convert_alpha()
         self.rect = pygame.Rect(100, 100, 100, 100)
+        self.current_image = 0
+
+    def update(self, *args):
+        def move_player(self):
+            key = pygame.key.get_pressed()
+            if key[pygame.K_d]:
+                self.rect[0] += GAME_SPEED
+            if key[pygame.K_a]:
+                self.rect[0] -= GAME_SPEED
+            self.current_image = (self.current_image + 1) % 10
+            self.image = self.image_run[self.current_image]
+            self.image = pygame.transform.scale(self.image,[100, 100])
+        move_player(self)
+
+
+class Ground(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('sprites/ground.png').convert_alpha()
+        self.image = pygame.transform.scale(self.image,(GROUND_WIDTH, GROUND_HEIGHT))
+        self.rect = self.image.get_rect()
 
 pygame.init()
-game_window =pygame.display.set_mode(WIDTH, HEIGHT)
+game_window = pygame.display.set_mode([WIDTH, HEIGHT])
 pygame.display.set_caption('Jogo 01')
+
+BACKGROUND = pygame.image.load('sprites/background_03.jpg')
+BACKGROUND = pygame.transform.scale(BACKGROUND,[WIDTH,HEIGHT])
+
+playerGroup = pygame.sprite.Group()
+player = Player()
+playerGroup.add(player)
 
 gameloop = True
 
 def draw():
-    game_window.fill(255, 255, 0)
+    playerGroup.draw(game_window)
 
+def update():
+    playerGroup.update()
+clock = pygame.time.Clock()
 while gameloop:
+    clock.tick(30)
+    game_window.blit(BACKGROUND, (0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.QUIT()
+            pygame.quit()
             break
+    update()
     draw()
     pygame.display.update()
